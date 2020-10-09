d8bb03c22cfa7a4833058091de799a6b12ca50b8
Siwat Sirichai <siwat@siwatinc.com>
2020-10-10 00:33:14 +0700

    Update __init__.py

--- osutazerj/__init__.py
+++ osutazerj/__init__.py
@@ -1,7 +1,7 @@
-from .osutazerj import OsuTazerJ
+from .osutazerj import osutazerj
 
 __red_end_user_data_statement__ = "I won't be responsible for death!"
 
 
 def setup(bot):
-    bot.add_cog(OsuTazerJ(bot))
+    bot.add_cog(osutazerj(bot))
